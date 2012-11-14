b042c3ee759831700df3a411897452e4bd3e3211
jwhicks <hicks.john@gmail.com>
2012-11-14 07:57:51 -0600

    Finalizing results
    Finalizing results and removing enhancements to input from file. This
    will be done in a future update to this file.

--- MIT Projects/IAP Assn3/Marathon.java
+++ MIT Projects/IAP Assn3/Marathon.java
@@ -2,27 +2,13 @@
  *
  * @author HicksJ
  */
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.File;  
-import java.util.Scanner;
-import java.io.IOException;  
+
+  
 import java.lang.String;
-import java.io.FileNotFoundException;
-import java.lang.Object;
 import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.LineNumberReader;
 
 
-public class Marathon {
-
-    /**
-     * @param args the command line arguments
-     */   
+public class Hello {  
 
 public static void main(String arg[]) {
     
@@ -41,11 +27,9 @@ public static void main(String arg[]) {
         341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
         343, 317, 265
     };   
-        
-         getNames();
-    
+                        
          for (int i = 0; i < names.length; i++) {
-           System.out.println("Original: "+i + ": "+ names[i]+ ": " + origTimes[i]);        
+           System.out.println("Input: "+ names[i]+ ": " + origTimes[i]);        
          }
            
          int[] times2 = sortTimes(times);
@@ -54,32 +38,17 @@ public static void main(String arg[]) {
            System.out.println("");           
          for (int i = 0; i < names.length; i++) {      
            System.out.println("Final Results: "+ names[sortedNames[i]]+ ": " + times2[i]);
-         }
+        }
       
-}
+}//Main
 
-   public static int[] matchNames(int[] orig, int[] sorted){
-       
- /*      input sorted integer array 
-  *      match item i to index in original array
-  *      save array of matched indexes and export this array
-  *     
- */           
-       int[] new1 = new int[sorted.length];
-       
-       for (int i = 0; i < sorted.length; i++) {
-          for (int j = 0; j < sorted.length; j++) {
-            if(orig[j] == sorted[i]){
-              new1[i] = j;
-           }
-         }
-       }
-           
-     return new1;
-   }
-   
-   public static int[] sortTimes(int[] times2){
+  public static int[] sortTimes(int[] times2){
   
+  /* 
+   * Sort the input array and copy into returned array
+   * 
+   */
+       
        int[] sortedTimes = new int[times2.length];
        
         Arrays.sort(times2);
@@ -87,43 +56,27 @@ public static void main(String arg[]) {
        
        return sortedTimes;     
       
-   }
+   }//sortTimes
 
-   public static String getNames(){
+   public static int[] matchNames(int[] orig, int[] sorted){
        
-        String text = "";
-        int count = 0;
-        List<String[]> list = new ArrayList<String[]>();
-        
-        File file = new File("C:\\Users\\hicksj\\Documents\\NetBeansProjects"
-                            +"\\Test\\src\\marathon.txt");
-        
-        try{
-            Scanner scanner = new Scanner(file);
-                                  
-            while ((scanner.hasNextLine())) {
-                String line = scanner.nextLine();                          
-                list.add(parseNames(line));                         
-            }
-            scanner.close();
-       }
-       catch (FileNotFoundException e){
-           System.err.format("Exception: " + e.getMessage());
-           e.printStackTrace();
-       }
-       for (String[] stringArr : list) {
-        for(String str : stringArr){
-            System.out.print(str+" ");
-        }
-        System.out.println("");
-    }
+ /*      
+  *      input sorted integer array 
+  *      match item i to index in original array
+  *      save array of matched indexes and export this array
+  *     
+ */           
+       int[] newNames = new int[sorted.length];
        
-       return text;
-   }
-   
-   public static String[] parseNames(String s){
-    String[] array = s.split("\\d");
-    return array;
-}
-
-  }
+       for (int i = 0; i < sorted.length; i++) {
+          for (int j = 0; j < sorted.length; j++) {
+            if(orig[j] == sorted[i]){
+              newNames[i] = j;
+           }
+         }
+       }
+           
+     return newNames;
+   }//matchNames
+         
+}//Class
